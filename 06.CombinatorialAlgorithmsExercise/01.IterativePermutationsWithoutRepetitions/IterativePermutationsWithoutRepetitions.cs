9ecb8f6b0708e2af9d9ba6a31b968a9f3b22c6cd
krasi070 <krasi0707@gmail.com>
2017-07-25 16:47:43 +0300

    added first two problems from combinatorial algos exercise

---
+++ 06.CombinatorialAlgorithmsExercise/01.IterativePermutationsWithoutRepetitions/IterativePermutationsWithoutRepetitions.cs
@@ -0,0 +1,49 @@
+﻿namespace _01.IterativePermutationsWithoutRepetitions
+{
+    using System;
+    using System.Linq;
+
+    public class IterativePermutationsWithoutRepetitions
+    {
+        public static void Main()
+        {
+            string[] arr = Console.ReadLine()
+                .Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                .ToArray();
+            PrintPermutations(arr);
+        }
+
+        private static void PrintPermutations(string[] arr)
+        {
+            int[] p = new int[arr.Length + 1];
+            for (int index = 0; index < p.Length; index++)
+            {
+                p[index] = index;
+            }
+
+            Console.WriteLine(string.Join(" ", arr));
+            int i = 1;
+            int j = 0;
+            while (i < arr.Length)
+            {
+                p[i]--;
+                j = i % 2 * p[i];
+                Swap(arr, i, j);
+                Console.WriteLine(string.Join(" ", arr));
+                i = 1;
+                while (p[i] == 0)
+                {
+                    p[i] = i;
+                    i++;
+                }
+            }
+        }
+
+        private static void Swap(string[] arr, int i, int j)
+        {
+            string temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+        }
+    }
+}
---
+++ 06.CombinatorialAlgorithmsExercise/02.IterativeCombinationsWithoutRepetition/IterativeCombinationsWithoutRepetition.cs
@@ -0,0 +1,70 @@
+﻿namespace _02.IterativeCombinationsWithoutRepetition
+{
+    using System;
+    using System.Linq;
+
+    public class IterativeCombinationsWithoutRepetition
+    {
+        public static void Main()
+        {
+            string[] elements = Console.ReadLine()
+                .Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                .ToArray();
+            int k = int.Parse(Console.ReadLine());
+            PrintCombinations(elements, k);
+        }
+
+        private static void PrintCombinations(string[] arr, int k)
+        {
+            int n = arr.Length;
+            CombinationElement[] combinations = new CombinationElement[k];
+            int i = 0;
+            int j = 0;
+            while (true)
+            {
+                if (j >= k)
+                {
+                    Console.WriteLine(string.Join<CombinationElement>(" ", combinations));
+                    int valToLowerJ = 1;
+                    while (k >= valToLowerJ && combinations[k - valToLowerJ].Value == arr[n - valToLowerJ])
+                    {
+                        valToLowerJ++;
+                    }
+
+                    j -= valToLowerJ;
+                    if (j < 0)
+                    {
+                        break;
+                    }
+
+                    if (i == n)
+                    {
+                        i = combinations[j].OriginalIndex + 1;
+                    }
+                }
+
+                combinations[j] = new CombinationElement(i, arr[i]);
+                i++;
+                j++;
+            }
+        }
+    }
+
+    public class CombinationElement
+    {
+        public CombinationElement(int i, string val)
+        {
+            this.OriginalIndex = i;
+            this.Value = val;
+        }
+
+        public int OriginalIndex { get; set; }
+
+        public string Value { get; set; }
+
+        public override string ToString()
+        {
+            return this.Value;
+        }
+    }
+}
