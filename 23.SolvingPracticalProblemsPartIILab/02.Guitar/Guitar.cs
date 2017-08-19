749f4078fa69563c3f6c952b43f30d4480851795
krasi070 <krasi0707@gmail.com>
2017-08-19 20:03:49 +0300

    added practical problems part 2 lab

---
+++ 23.SolvingPracticalProblemsPartIILab/02.Guitar/Guitar.cs
@@ -0,0 +1,51 @@
+﻿namespace _02.Guitar
+{
+    using System;
+    using System.Linq;
+
+    public class Guitar
+    {
+        public static void Main()
+        {
+            int[] volumeChanges = Console.ReadLine()
+                .Split(new[] { ',', ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+            int startingVolume = int.Parse(Console.ReadLine());
+            int maxVolume = int.Parse(Console.ReadLine());
+            bool[,] matrix = new bool[volumeChanges.Length + 1, maxVolume + 1];
+
+            matrix[0, startingVolume] = true;
+            for (int i = 1; i < matrix.GetLength(0); i++)
+            {
+                int volumeChange = volumeChanges[i - 1];
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    if (matrix[i - 1, j] == true)
+                    {
+                        if (j - volumeChange >= 0)
+                        {
+                            matrix[i, j - volumeChange] = true;
+                        }
+
+                        if (j + volumeChange <= maxVolume)
+                        {
+                            matrix[i, j + volumeChange] = true;
+                        }
+                    }
+                }
+            }
+
+            int maxVolumeForLastSong = -1;
+            for (int i = 0; i < matrix.GetLength(1); i++)
+            {
+                if (matrix[volumeChanges.Length, i])
+                {
+                    maxVolumeForLastSong = i;
+                }
+            }
+
+            Console.WriteLine(maxVolumeForLastSong);
+        }
+    }
+}
---
+++ 23.SolvingPracticalProblemsPartIILab/03.ShortestPath/ShortestPath.cs
@@ -0,0 +1,45 @@
+﻿namespace _03.ShortestPath
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class ShortestPath
+    {
+        private static List<string> paths = new List<string>();
+
+        public static void Main()
+        {
+            string path = Console.ReadLine();
+
+            GenerateAllPossibleMissingSymbols(path.ToCharArray());
+            Console.WriteLine(paths.Count);
+            Console.WriteLine(string.Join(Environment.NewLine, paths));
+        }
+
+        private static void GenerateAllPossibleMissingSymbols(char[] path, int index = 0)
+        {
+            if (index >= path.Length)
+            {
+                paths.Add(string.Join("", path));
+            }
+            else
+            {
+                if (path[index] == '*')
+                {
+                    string symbols = "LRS";
+                    for (int i = 0; i < symbols.Length; i++)
+                    {
+                        path[index] = symbols[i];
+                        GenerateAllPossibleMissingSymbols(path, index + 1);
+                    }
+
+                    path[index] = '*';
+                }
+                else
+                {
+                    GenerateAllPossibleMissingSymbols(path, index + 1);
+                }              
+            }
+        }
+    }
+}
